058907f9567295526ff49b517e1757e5172833ae
idkgorapeme <309462509+idkgorapeme@users.noreply.github.com>
2026-08-04 16:54:03 +0000

    Remove all MM2 features
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- src/init.lua
+++ src/init.lua
@@ -77,7 +77,7 @@ env.BrainrotPolice.globals = {
     "AutoWin", "KeyFarm", "KeyHighlight", "World2Help", "World2Destroy",
     "MacroPlaying", "MacroRecording",
     "BPFly", "BPInfJump", "BPNoclip", "BPAntiAFK", "AutoPrestige", "AutoClickAttack",
-    "DBCollect", "DBUpgrade", "DBRebirth", "SBPump", "SBUpgrade", "SBLaunch", "SBFuel", "MNLock", "MNMerge", "SSWin", "SSRebirth", "KEWin1", "KEWin2", "KEDestroy", "KECoins", "KEBuy", "KSWalk", "KSBuy", "KSWin", "KSDestroy", "SWSell", "SWRebirth", "SWCollect", "WMRebirth", "WMPrestige", "WMPlaytime", "MMPrestige", "MMCrates", "MMEvents", "BBTrajectory", "BBHighlight", "MMCoins", "MMCoinEsp", "MMCodes", "MMQueue"
+    "DBCollect", "DBUpgrade", "DBRebirth", "SBPump", "SBUpgrade", "SBLaunch", "SBFuel", "MNLock", "MNMerge", "SSWin", "SSRebirth", "KEWin1", "KEWin2", "KEDestroy", "KECoins", "KEBuy", "KSWalk", "KSBuy", "KSWin", "KSDestroy", "SWSell", "SWRebirth", "SWCollect", "WMRebirth", "WMPrestige", "WMPlaytime", "BBTrajectory", "BBHighlight"
 }
 
 function env.BrainrotPolice.unload()
